43c827f7541b8247e660b9575673feef7dbc6731
Shubham Rohan Asthana <asthana.st.francis@gmail.com>
2014-02-12 21:16:24 +0530

    Create ip_addr.py

---
+++ ip_addr.py
@@ -0,0 +1,22 @@
+#Assigning the IP addresses to the laptops acting as switches
+S1="10.200.200.145"
+S2="10.200.200.161"
+S3="10.200.200.177"
+
+
+#Assigning IP addresses to the hosts connected to the latops
+
+#hosts connected to Switch1 
+h1="10.200.200.149"
+h2="10.200.200.150"
+h3="10.200.200.155"
+#hosts connected to Switch2
+h4="10.200.200.163"
+h5="10.200.200.167"
+h6="10.200.200.172"
+h7="10.200.200.175"
+#hosts connected to Switch3
+h8="10.200.200.180"
+h9="10.200.200.181"
+h10="10.200.200.182"
+h11="10.200.200.183"
